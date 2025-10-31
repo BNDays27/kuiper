c571c5851f0d7a495910dd711570d6aacb448121
BNDays27 <brionical@proton.me>
2025-10-31 11:50:06 +0000

    updated the dots yay

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # Installs the Chaotic AUR since I use it to install stuff like Zen and ungoogled chromium
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
@@ -13,19 +15,21 @@ Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
 sudo pacman -Syu --noconfirm
 
 # Installs all of the dependencies and other stuff I use
-sudo pacman -S stow plasma kitty konsole dolphin qt6ct-kde hyprland xdg-desktop-portal-hyprland hyprpaper eww hyprlock zen-browser-bin ungoogled-chromium-bin chromium-extension-web-store chromium-widevine neovim wl-clipboard hyprshot fzf paru-git git jq zsh elisa pipewire-alsa ark unrar unzip wget ttf-jetbrains-mono ttf-jetbrains-mono-nerd --needed --noconfirm
+sudo pacman -Syu stow plasma kitty konsole dolphin qt6ct-kde hyprland xdg-desktop-portal-hyprland hyprpaper eww hyprlock zen-browser-bin ungoogled-chromium-bin chromium-extension-web-store chromium-widevine neovim wl-clipboard hyprshot fzf paru-git git jq zsh elisa pipewire-alsa ark unrar unzip wget ttf-jetbrains-mono ttf-jetbrains-mono-nerd --needed --noconfirm
 
-# installs the QT theme for KDE
+# installs the QT theme for KDE, I use Catppuccin so its catppuccin/kde
 mkdir $HOME/.git
 cd $HOME/.git
 git clone --depth=1 https://github.com/catppuccin/kde catppuccin-kde 
 cd catppuccin-kde
 ./install.sh
 
-# installs the GTK theme
+# installs the GTK theme, Catppuccin used to have one but that's archived now so I'm using Fausto-Korpsvart/Catppuccin-GTK-Theme
 cd $HOME/.git
 git clone https://github.com/Fausto-Korpsvart/Catppuccin-GTK-Theme.git catppuccin-gtk
 cd catppuccin-gtk/themes
 ./install.sh --tweaks macos -t mauve
 
-git 
+cd $SCRIPT_DIR
+rm -rf $HOME/.config/*
+stow .
