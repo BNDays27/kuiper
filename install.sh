fe146fef4e7878d4191a8312f030ed74a01530c6
BNDays27 <brionical@proton.me>
2025-10-31 11:45:17 +0000

    added qt6ct config, plus also a shell script but that aint finished yet

--- install.sh
+++ install.sh
@@ -13,4 +13,19 @@ Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
 sudo pacman -Syu --noconfirm
 
 # Installs all of the dependencies and other stuff I use
-sudo pacman -S plasma kitty konsole dolphin qt6ct-kde hyprland xdg-desktop-portal-hyprland hyprpaper eww hyprlock zen-browser-bin ungoogled-chromium-bin chromium-extension-web-store chromium-widevine neovim wl-clipboard hyprshot fzf paru-git git jq zsh elisa pipewire-alsa
+sudo pacman -S stow plasma kitty konsole dolphin qt6ct-kde hyprland xdg-desktop-portal-hyprland hyprpaper eww hyprlock zen-browser-bin ungoogled-chromium-bin chromium-extension-web-store chromium-widevine neovim wl-clipboard hyprshot fzf paru-git git jq zsh elisa pipewire-alsa ark unrar unzip wget ttf-jetbrains-mono ttf-jetbrains-mono-nerd --needed --noconfirm
+
+# installs the QT theme for KDE
+mkdir $HOME/.git
+cd $HOME/.git
+git clone --depth=1 https://github.com/catppuccin/kde catppuccin-kde 
+cd catppuccin-kde
+./install.sh
+
+# installs the GTK theme
+cd $HOME/.git
+git clone https://github.com/Fausto-Korpsvart/Catppuccin-GTK-Theme.git catppuccin-gtk
+cd catppuccin-gtk/themes
+./install.sh --tweaks macos -t mauve
+
+git 
