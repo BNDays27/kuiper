990a54f05ca7438083715b8ce51be9081a95bd17
BNDays27 <brionical@proton.me>
2025-11-02 11:07:23 +0000

    added the dots yay

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+#############################
+### SCRIPT TIME YAYYYYYYY ###
+#############################
+
+# just prefacing here you will need to be near your pc as this script won't automatically install stuff like your QT theme, so be ready to change anything
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# Installs the Chaotic AUR since I use it to install stuff like Zen and ungoogled chromium
+# Installs the Chaotic AUR repo since I use it to install stuff like Zen and ungoogled chromium easier
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 
@@ -15,21 +20,34 @@ Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
 sudo pacman -Syu --noconfirm
 
 # Installs all of the dependencies and other stuff I use
-sudo pacman -Syu stow plasma kitty konsole dolphin qt6ct-kde hyprland xdg-desktop-portal-hyprland hyprpaper eww hyprlock zen-browser-bin ungoogled-chromium-bin chromium-extension-web-store chromium-widevine neovim wl-clipboard hyprshot fzf paru-git git jq zsh elisa pipewire-alsa ark unrar unzip wget ttf-jetbrains-mono ttf-jetbrains-mono-nerd --needed --noconfirm
+sudo pacman -Syu stow plasma kitty konsole flatpak discover dolphin qt6ct-kde hyprland xdg-desktop-portal-hyprland hyprpaper eww hyprlock zen-browser-bin ungoogled-chromium-bin chromium-extension-web-store chromium-widevine neovim wl-clipboard hyprshot fzf paru-git git jq zsh elisa pipewire-alsa ark unrar unzip wget ttf-jetbrains-mono ttf-jetbrains-mono-nerd btop rocm-smi-lib fastfetch --needed --noconfirm
 
-# installs the QT theme for KDE, I use Catppuccin so its catppuccin/kde
+# Installs the QT theme for KDE, I use Catppuccin so its catppuccin/kde
 mkdir $HOME/.git
 cd $HOME/.git
 git clone --depth=1 https://github.com/catppuccin/kde catppuccin-kde 
 cd catppuccin-kde
 ./install.sh
 
-# installs the GTK theme, Catppuccin used to have one but that's archived now so I'm using Fausto-Korpsvart/Catppuccin-GTK-Theme
+# Installs the GTK theme, Catppuccin used to have one but that's archived now so I'm using Fausto-Korpsvart/Catppuccin-GTK-Theme
 cd $HOME/.git
 git clone https://github.com/Fausto-Korpsvart/Catppuccin-GTK-Theme.git catppuccin-gtk
 cd catppuccin-gtk/themes
 ./install.sh --tweaks macos -t mauve
 
+# So Flatpaks use the GTK theme
+sudo flatpak override --filesystem=$HOME/.themes
+
+# Btop theme, again uses Catppuccin, but this time im symlinking because that's easier
+cd $HOME/.git
+git clone https://github.com/catppuccin/btop catppuccin-btop
+mkdir -p $HOME/.config/btop/
+ln -s $HOME/.git/catppuccin-btop/themes $HOME/.config/btop/themes
+
+# changes the shell to zsh, you CAN use bash if you want but I dont use it, so i just have my aliases in there, the OMP theme, the fastfetch thing and that's it
+chsh -s /bin/zsh
+
 cd $SCRIPT_DIR
 rm -rf $HOME/.config/*
 stow .
+echo "okay, we're done, have a fun time using the dots!"
