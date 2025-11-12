ecaa5f3a27743c5a5002dd31fbb571fb11768833
BNDays27 <brionical@proton.me>
2025-11-12 12:59:14 +0000

    updated the dots, also worked on the script a lil

--- install.sh
+++ install.sh
@@ -6,6 +6,20 @@
 # just prefacing here you will need to be near your pc as this script won't automatically install stuff like your QT theme, so be ready to change anything
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "hey! You will need to be alert for the install, some of this isn't automatic so don't go make a coffee or something when installing this
+1. Cool, I can do that right now
+2. damn, that aint gonna be possible"
+read option
+
+accept=1
+
+if [$(echo $option) == $(echo $accept)]; then
+	echo "cool, have fun!"
+	exit 0
+else
+	echo "well then bye"
+	exit 0
+fi
 
 # Installs the Chaotic AUR repo since I use it to install stuff like Zen and ungoogled chromium easier
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
@@ -20,12 +34,12 @@ Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
 sudo pacman -Syu --noconfirm
 
 # Installs all of the dependencies and other stuff I use
-sudo pacman -Syu stow plasma kitty konsole flatpak discover dolphin qt6ct-kde hyprland xdg-desktop-portal-hyprland hyprpaper eww hyprlock zen-browser-bin ungoogled-chromium-bin chromium-extension-web-store chromium-widevine neovim wl-clipboard hyprshot fzf paru-git git jq zsh elisa pipewire-alsa ark unrar unzip wget ttf-jetbrains-mono ttf-jetbrains-mono-nerd btop rocm-smi-lib fastfetch mpd rmpc mpd-mpris steam hyprpicker wlogout power-profiles-daemon noto-fonts-cjk spotify spicetify-cli spicetify-marketplace papirus-icon-theme --needed --noconfirm
+sudo pacman -Syu stow plasma kitty konsole flatpak discover dolphin qt6ct-kde hyprland xdg-desktop-portal-hyprland hyprpaper eww hyprlock zen-browser-bin ungoogled-chromium-bin chromium-extension-web-store chromium-widevine neovim wl-clipboard hyprshot fzf paru-git git jq zsh elisa pipewire-alsa ark unrar unzip wget ttf-jetbrains-mono ttf-jetbrains-mono-nerd btop rocm-smi-lib fastfetch mpd rmpc steam hyprpicker wlogout power-profiles-daemon noto-fonts-cjk spotify spicetify-cli spicetify-marketplace papirus-icon-theme sddm --needed --noconfirm
 
 # installing AUR packages with paru
-paru -S btop-theme-catppuccin plymouth-theme-catppuccin-mocha-git papirus-folders-catppuccin-git --noconfirm catppuccin-sddm-theme-mocha --needed
+paru -S btop-theme-catppuccin mpdris2 plymouth-theme-catppuccin-mocha-git papirus-folders-catppuccin-git catppuccin-sddm-theme-mocha --noconfirm --needed
 
-# sets Papirus' & Plymouth's theme to Catppuccin Mocha
+# sets Papirus' & Plymouth's theme to Catppuccin Mocha (specifically mauve for papirus)
 plymouth-set-default-theme -R catppuccin-mocha
 papirus-icon-theme -C cat-mocha-mauve --theme Papirus-Dark
 
@@ -47,19 +61,16 @@ cd catppuccin-gtk/themes
 sudo flatpak override --filesystem=$HOME/.themes
 
 # So mpd and mpd-mpris launch now and on startup
-systemctl enable --now mpd mpd-mpris
-
-# Btop theme, again uses Catppuccin, but this time im symlinking because that's easier
-cd $HOME/.git
-git clone https://github.com/catppuccin/btop catppuccin-btop
-mkdir -p $HOME/.config/btop/
-ln -s $HOME/.git/catppuccin-btop/themes $HOME/.config/btop/themes
+systemctl enable --now mpd mpDris2
 
 # changes the shell to zsh, you CAN use bash if you want but I dont use it, so i just have my aliases in there, the OMP theme, the fastfetch thing and that's it
 chsh -s /bin/zsh
 
+# stows the dotfiles
 cd $SCRIPT_DIR
 rm -rf $HOME/.config/*
 stow .
 echo "okay, we're done, have a fun time using the dots!
-You will need to set up Spotify & spicetify manually, as there's no way for me to set it up manually"
+You will need to set up Spotify & spicetify manually, as there's no way for me to set it up manually
+maybe i'll try automate more of this, but dont count on it!
+"
